4d3bbf95cc55c61db86c5d68d10eeae9a57fee64
Todd Page <twpage@gmail.com>
2014-09-05 17:06:18 -0400

    changed version to account for save files
    version number was too big and messed up savegame file header

--- Brogue Source/BrogueCode/Rogue.h
+++ Brogue Source/BrogueCode/Rogue.h
@@ -31,7 +31,7 @@
 #define USE_UNICODE
 
 // version string -- no more than 16 bytes:
-#define BROGUE_VERSION_STRING "Archive Scores Mod v0.1 based on Brogue 1.7.4"
+#define BROGUE_VERSION_STRING "1.7.4_BF02"
 
 // debug macros -- define DEBUGGING as 1 to enable wizard mode.
 
