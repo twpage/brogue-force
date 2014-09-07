80f2989dff35dca91e96e9bd71d98d9e90f64b22
Todd Page <twpage@gmail.com>
2014-09-07 10:44:42 -0400

    Fixed version number and updated title menu
    version number was causing a bug where save files could not be restored

--- Brogue Source/BrogueCode/Rogue.h
+++ Brogue Source/BrogueCode/Rogue.h
@@ -31,7 +31,8 @@
 #define USE_UNICODE
 
 // version string -- no more than 16 bytes:
-#define BROGUE_VERSION_STRING "1.7.4_BF02"
+#define BROGUE_VERSION_STRING "1.7.4"
+#define BROGUE_MOD_DESCRIPTION "Archive Scores Mod v0.1 based on Brogue 1.7.4"
 
 // debug macros -- define DEBUGGING as 1 to enable wizard mode.
 
